64bf7ce78cf1edb958314df897ba21ce74980a2c
kseniailina <50660900+kseniailina@users.noreply.github.com>
2020-09-11 11:12:12 +0300

    Update Project.kt

--- .teamcity/KotlinRoot_KotlinSubpr/Project.kt
+++ .teamcity/KotlinRoot_KotlinSubpr/Project.kt
@@ -5,11 +5,13 @@ import KotlinRoot_KotlinSubpr.vcsRoots.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.Project
 
+const val REPOSITORY_NAME = "Kotlin Subpr changed name"
+
 object Project : Project({
     uuid = "9710729e-3de6-461a-92e2-d0223762e836"
     id("KotlinRoot_KotlinSubpr")
     parentId("KotlinRoot")
-    name = "Kotlin Subpr"
+    name = REPOSITORY_NAME
 
     vcsRoot(KotlinRoot_KotlinSubpr_HttpsGithubComKseniailinaKotlinSubprGitRefsHeadsMaster)
 
